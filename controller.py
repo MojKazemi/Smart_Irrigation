6634a1a57c34d98444eb50a75b84cc92f0322ed2
Mojtaba Kazemi <Moj.kazemii@gmail.com>
2023-04-01 16:22:01 +0100

    add manual func

--- controller.py
+++ controller.py
@@ -30,15 +30,50 @@ class MoistController:
         self.IDs['farm'] = msg['farmID']
         self.IDs['section'] = msg['sectionID']
 
-        if msg['e']['n'] == 'Temperature':
-            tmp_value = msg['e']['value']
-            self.temp_values.append(tmp_value)
-        elif msg['e']['n'] == 'Soil_Moisture':
-            mois_value = msg['e']['value']
-            self.mois_values.append(mois_value)
+        control_status = self.my_req.get_control_status(self.IDs)
+        if control_status == 'auto':
+            if msg['e']['n'] == 'Temperature':
+                tmp_value = msg['e']['value']
+                self.temp_values.append(tmp_value)
+            elif msg['e']['n'] == 'Soil_Moisture':
+                mois_value = msg['e']['value']
+                self.mois_values.append(mois_value)
 
-        if len(self.temp_values) == len(self.mois_values) and len(self.mois_values) >1:
-            self.ControlServo(self.temp_values, self.mois_values)
+            if len(self.temp_values) == len(self.mois_values) and len(self.mois_values) >1:
+                self.ControlServo(self.temp_values, self.mois_values)
+
+        elif control_status == 'manual':
+            self.manual_control()
+            
+    def manual_control(self):
+        _schedul = self.my_req.get_manual_schedul(self.IDs)
+        if _schedul["timers"] != []:
+            for period in _schedul["timers"]:
+                print(period["days"])
+                days = period["days"][1:] + [period["days"][0]]
+                print(days)
+                weekday = [i for i in range(len(days)) if days[i] == 1]
+                for i in weekday:
+                    _time_now = datetime.datetime.now()
+                    if i == _time_now.weekday():
+                        _time_start = datetime.datetime.fromtimestamp(period["starttime"]/1000)
+                        _time_stop = datetime.datetime.fromtimestamp(period["endtime"]/1000)
+                        if _time_start.hour <= _time_now.hour <= _time_stop.hour:
+                            if _time_start.minute <= _time_now.minute <= _time_stop.minute:
+                                self.status = 'on'
+                            else:
+                                    self.status = 'off'
+                        else:
+                            self.status = 'off'
+
+                        prev_status = self.my_req.get_status(self.IDs)
+
+                        if self.status != prev_status:
+                            self.my_req.put_status(self.IDs,self.status)
+                            print(f'------>>>> become {self.status} <<<<<--------')
+                            self.sendActStatus(self.status)
+
+        
 
     def ControlServo(self,temp_values, mois_values):
         if len(temp_values) > self.pre_times:
