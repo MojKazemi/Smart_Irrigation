5f02e052144e5c073634dd2bb2972c86b624c599
Mojtaba Kazemi <Moj.kazemii@gmail.com>
2023-05-01 15:18:34 +0100

    solve bug of month

--- cons_cal.py
+++ cons_cal.py
@@ -66,16 +66,22 @@ class consumption_calc:
         if mm.isdigit() and len(mm) == 1:
             month_year = f"0{mm}/" + month_year.split("/")[1]
 
-        start_time = f'01/{month_year} 00:00:00'
-        month = month_year.split('/')[0]
-        if month == '02':
-            end_day = '28'
-        elif month == '04'or month == '06' or month == '09' or month == '11':
-            end_day = '30'
+        if int(month_year.split('/')[0]) == datetime.now().month and int(month_year.split('/')[1]) == datetime.now().year:
+            now_time = datetime.now()
+            end_time = now_time.strftime('%d/%m/%Y %H:%M:%S')
+
         else:
-            end_day = '31'
-        
-        end_time = f'{end_day}/{month_year} 23:59:59'
+            month = month_year.split('/')[0]
+            if month == '02':
+                end_day = '28'
+            elif month == '04'or month == '06' or month == '09' or month == '11':
+                end_day = '30'
+            else:
+                end_day = '31'
+
+            end_time = f'{end_day}/{month_year} 23:59:59'
+
+        start_time = f'01/{month_year} 00:00:00'
         on_sec, power_cons = self.selective_time(start_time,end_time)
 
         return on_sec, power_cons
@@ -94,7 +100,7 @@ class consumption_calc:
 if __name__ == "__main__":
 
     IDs = {
-    'farmID': 'Farm_1',
+    'farmID': 'Farm_2',
     'sectionID': 'Section1'
     }
 
@@ -108,7 +114,7 @@ if __name__ == "__main__":
     # on_sec, power_cons = cons.daily_usage('25/03/2023')
     # print(f'on_sec: {on_sec}\npower cons: {power_cons}')
 
-    on_sec, power_cons = cons.monthly_usage(month_year = '4/2023')
+    on_sec, power_cons = cons.monthly_usage(month_year = '5/2023')
     print(f'on_sec: {on_sec}\npower cons: {power_cons}')
 
     # _dict = cons.yearly(year = '2024')
